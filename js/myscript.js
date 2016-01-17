c1ad4055c1ad4cf1cb9f63cc2d460d29d07de29a
recasanova <regina.casanova.90@gmail.com>
2016-01-16 22:13:58 -0500

    Cambios en el menú
    El menú ya resalta donde es que se encuentra

--- js/myscript.js
+++ js/myscript.js
@@ -1,15 +1,42 @@
-$(document).ready(function() { 
-
-	$(".timeline-item").hover(function () {
-	    $(".timeline-item").removeClass("active");
-	    $(this).toggleClass("active");
-	    $(this).prev(".timeline-item").toggleClass("close");
-	    $(this).next(".timeline-item").toggleClass("close");
-	});
-
-	$(".nav a").on("click", function(){
-	   $(".nav").find(".active").removeClass("active");
-	   $(this).parent().addClass("active");
-	});
-
+$(".timeline-item").hover(function () {
+    $(".timeline-item").removeClass("active");
+    $(this).toggleClass("active");
+    $(this).prev(".timeline-item").toggleClass("close");
+    $(this).next(".timeline-item").toggleClass("close");
 });
+
+function GetCurrentPageName() { 
+	//method to get Current page name from url. 
+	var PageURL = document.location.href; 
+	var PageName = PageURL.substring(PageURL.lastIndexOf('/') + 1); 
+	 
+	return PageName.toLowerCase() ;
+	}
+	 
+	$(document).ready(function(){
+	var CurrPage = GetCurrentPageName();
+
+	console.log(CurrPage);
+	 
+	switch(CurrPage){
+	case 'index.php':
+	 $('#li_home').addClass('active') ;
+	 break;
+	case 'educacion.php':
+	 $('#li_educacion').addClass('active') ;
+	 break;
+	case 'experiencia.php':
+	 $('#li_experiencia').addClass('active') ;
+	 break;
+	case 'web.php':
+	 $('#li_portafolio').addClass('active') ;
+	 break;
+	case 'fotografia.php':
+	 $('#li_portafolio').addClass('active') ;
+	 break;
+	case 'contacto.php':
+	 $('#li_contacto').addClass('active') ;
+	 break;
+	}
+});
+
--- menu.php
+++ menu.php
@@ -1,5 +1,6 @@
 <div class="header">
 	<!-- Basado en el ejemplo del NAV-BAR de Bootstrap, https://getbootstrap.com/components/#navbar -->
+
 	<nav class="navbar navbar-inverse navbar-custom">
 		<div class="container-fluid no-padding">
     	<!-- Brand and toggle get grouped for better mobile display -->
@@ -15,23 +16,24 @@
 
 			<!-- Collect the nav links, forms, and other content for toggling -->
 		    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-    	    	<ul class="nav navbar-nav navbar-right text-white menu">
-			        <li class="active"><a href="index.php">Acerca de<br><i class="fa fa-user"></i></a></li>
-			        <li><a href="educacion.php">Preparación<br><i class="fa fa-graduation-cap"></i></a></li>
-			        <li><a href="experiencia.php">Experiencia<br><i class="fa fa-institution"></i></a></li>
-			        <li class="dropdown">
+    	    	<ul class="nav navbar-nav navbar-right text-white">
+			        <li id="li_home"><a href="index.php">Acerca de<br><i class="fa fa-user"></i></a></li>
+			        <li id="li_educacion"><a href="educacion.php">Preparación<br><i class="fa fa-graduation-cap"></i></a></li>
+			        <li id="li_experiencia"><a href="experiencia.php">Experiencia<br><i class="fa fa-institution"></i></a></li>
+			        <li id="li_portafolio" class="dropdown">
 			          <a href="web.php" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Portafolio<span class="caret"></span><br><i class="fa fa-suitcase"></i> </a>
 			          <ul class="dropdown-menu">
-			            <li><a href="web.php">Web<br><i class="fa fa-desktop"></i></a></li>
+			            <li id="li_web"><a href="web.php">Web<br><i class="fa fa-desktop"></i></a></li>
 			            <li role="separator" class="divider"></li>
-			            <li><a href="#">Fotografía<br><i class="fa fa-camera"></i></a></li>
+			            <li id="li_fotografia"><a href="#">Fotografía<br><i class="fa fa-camera"></i></a></li>
 			          </ul>
 			        </li>
-			        <li><a href="contacto.php">Contacto<br><i class="fa fa-envelope"></i></a></li>
+			        <li id="li_contacto"><a href="contacto.php">Contacto<br><i class="fa fa-envelope"></i></a></li>
 			     </ul>
 			</div><!-- /.navbar-collapse -->
   		</div><!-- /.container-fluid -->
 	</nav>
 </div>
+
 	
 	
